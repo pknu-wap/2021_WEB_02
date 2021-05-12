2428c8059a0b4b6e97a40f2ffc715aef4c8d56e9
ksy13001 <ksy13001@gmail.com>
2021-05-12 17:17:01 +0900

    페이지 관련 컨트롤러

--- src/main/java/com/web02/test/springboot/web/IndexController.java
+++ src/main/java/com/web02/test/springboot/web/IndexController.java
@@ -1,12 +1,20 @@
 package com.web02.test.springboot.web;
 
+import com.web02.test.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index"; // src/main/resources/templates/index.mustache
     }
 
@@ -14,4 +22,5 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
 }
