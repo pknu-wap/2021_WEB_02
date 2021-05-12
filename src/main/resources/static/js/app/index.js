3b31e10bc26b6c166fcee7e3fbcf076a821ee675
ksy13001 <ksy13001@gmail.com>
2021-05-12 20:15:30 +0900

    index.js

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,30 +1,76 @@
-var main ={                 // var index-> index 객체 안에서만 함수가 유효해서 다른 js랑 안 겹침
-    init : function(){
-      var _this = this;
-       $('#btn-save').on('click', function () {
-           _this.save();
-       });
-    },
-    save: function(){
-    var data={
-        title:$('#title').val(),
-        author:$('#author').val(),
-        content:$('#content').val()
-    };
+//api 호출 js
+var main = {    //다른 js랑 겹치지 않게
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {    //btn-save란 id 엘리먼트 클릭시 save 함수 실행
+            _this.save();
+        });
 
-    $.ajax({
-          type: 'POST',
-          url: '/api/v1/posts',
-          dataType: 'json',
-          contentType:'application/json; charset=utf-8',
-          data: JSON.stringify(data)
-    }).done(function(){
-        alert('글이 등록되었습니다.');
-        window.location.href='/';       //글 등록시 메인페이지로 이동
-    }).fail(function(error){
-        alert(JSON.stringify(error));
-    });
-  }
+        $('#btn-update').on('click', function () {  //수정 함수 실행
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {  //삭제 함수 실행
+            _this.delete();
+        });
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),   //jquery 객체 title 값 불러오기
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',       //등록(PostsApiController에서 정의함)
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',        //수정
+            url: '/api/v1/posts/'+id,   //id로 게시물 구분
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+
 };
 
 main.init();
