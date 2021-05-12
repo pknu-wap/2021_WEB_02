bff75c33fdaa992a43e228035770e74afff5f631
ksy13001 <ksy13001@gmail.com>
2021-05-12 17:09:09 +0900

    @Query 추가

--- src/main/java/com/web02/test/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/web02/test/springboot/domain/posts/PostsRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 // 인터페이스 생성후 JpaRepository<Entity 클래스, PK타입> 으로 CRUD 메소드 자동생성
 public interface PostsRepository extends JpaRepository<Posts,Long>{
 
-    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")      //@Query가 가독성이 더 좋음
     List<Posts> findAllDesc();
 
 }
