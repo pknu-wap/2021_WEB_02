286ca6d44a928140dca506c22e41edcb5a10c9ca
ksy13001 <ksy13001@gmail.com>
2021-05-12 20:42:01 +0900

    페이지 관련 컨트롤러

--- src/main/java/com/web02/test/springboot/web/IndexController.java
+++ src/main/java/com/web02/test/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.web02.test.springboot.web;
 
 import com.web02.test.springboot.service.posts.PostsService;
+import com.web02.test.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model;    //model 을 사용하여 View에 데이터 전달
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -12,15 +14,22 @@ public class IndexController {
 
     private final PostsService postsService;
 
-    @GetMapping("/")
-    public String index(Model model){
+    @GetMapping("/")    //   첫화면 index+ 글 조회
+    public String index(Model model){       //model: postService.findAllDesc()로 가져온 결과 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
         return "index"; // src/main/resources/templates/index.mustache
     }
 
-    @GetMapping("/posts/save")
-    public String postsSave(){
+    @GetMapping("/posts/save")  //글 등록화면 이동
+    public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")    //글 수정화면 이동,id로 구분
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
     }
 
 }
