3ee7b5ef5cf37265e215424353b89d322d4eca1a
ksy13001 <ksy13001@gmail.com>
2021-05-22 00:39:45 +0900

    MD5 체크섬 변환

---
+++ src/main/java/com/web02/springboot/web/util/MD5Generator.java
@@ -0,0 +1,27 @@
+//파일  MD5 체크섬의 값으로 서버에 저장
+package com.web02.springboot.web.util;
+
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Generator {
+    private String result;
+
+    public MD5Generator(String input) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        MessageDigest mdMD5 = MessageDigest.getInstance("MD5");
+        mdMD5.update(input.getBytes("UTF-8"));
+        byte[] md5Hash = mdMD5.digest();
+        StringBuilder hexMD5hash = new StringBuilder();
+        for(byte b : md5Hash) {
+            String hexString = String.format("%02x", b);
+            hexMD5hash.append(hexString);
+        }
+        result = hexMD5hash.toString();
+    }
+
+    public String toString() {
+        return result;
+    }
+}
