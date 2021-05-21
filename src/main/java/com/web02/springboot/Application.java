c1f8faafe289c8cd23cbb33df13004defb7214f3
ksy13001 <ksy13001@gmail.com>
2021-05-21 22:58:05 +0900

    Application 클래스

--- src/main/java/com/web02/springboot/Application.java
+++ src/main/java/com/web02/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.web02.test.springboot;
+package com.web02.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
