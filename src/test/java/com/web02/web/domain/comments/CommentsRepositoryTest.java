e61fa421a8ab42b4b49f18e845428569fa166790
ksy13001 <ksy13001@gmail.com>
2021-06-26 22:00:24 +0900

    댓글 테스트

---
+++ src/test/java/com/web02/web/domain/comments/CommentsRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.web02.web.domain.comments;
+
+
+import com.web02.domain.comments.Comments;
+import com.web02.domain.comments.CommentsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentsRepositoryTest {
+
+    @Autowired
+    CommentsRepository commentsRepository;
+
+    @After
+    public void cleanup(){
+        commentsRepository.deleteAll();
+    }
+
+    @Test
+    public void 댓글달기(){
+        //given
+        Long commentId= 11L;
+        String content="댓글 내용";
+        commentsRepository.save(Comments.builder()
+                .commentId(commentId)
+                .content(content)
+                .build());
+        //when
+        List<Comments> commentsList=commentsRepository.findAll();
+        //then
+        Comments comments = commentsList.get(0);
+        assertThat(comments.getCommentId()).isEqualTo(commentId);
+        assertThat(comments.getContent()).isEqualTo(content);
+    }
+    @Test
+    public void 댓글삭제(){
+        //given
+
+        //when
+
+        //then
+
+    }
+
+}
