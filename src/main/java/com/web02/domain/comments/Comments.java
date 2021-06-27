d008638e6577575eed366b335a9302f9294d242f
ksy13001 <ksy13001@gmail.com>
2021-06-28 05:35:12 +0900

    댓글 entity

--- src/main/java/com/web02/domain/comments/Comments.java
+++ src/main/java/com/web02/domain/comments/Comments.java
@@ -3,7 +3,6 @@ package com.web02.domain.comments;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.web02.domain.BaseTimeEntity;
 import com.web02.domain.posts.Posts;
-import com.web02.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,20 +16,26 @@ public class Comments extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="COMMENT_ID")
     private Long id;
-
-    @Column(nullable = false)
-    private Long commentId;
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
+    @Column
+    private String author;
+
+    @ManyToOne
+    @JoinColumn(name = "POSTS_ID")
+    private Posts posts;
+
 
     @Builder
-    public Comments(Long id, Long commentId, String content){
+    public Comments(Long id, String content,String author,Posts posts){
         this.id=id;
-        this.commentId=commentId;
         this.content=content;
+        this.author=author;
+        this.posts=posts;
     }
 
 }
