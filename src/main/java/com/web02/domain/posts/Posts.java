129c8145600172711931573da35403fabd3be388
ksy13001 <ksy13001@gmail.com>
2021-06-28 05:36:35 +0900

    .

--- src/main/java/com/web02/domain/posts/Posts.java
+++ src/main/java/com/web02/domain/posts/Posts.java
@@ -1,12 +1,14 @@
 package com.web02.domain.posts;
 
 import com.web02.domain.BaseTimeEntity;
+import com.web02.domain.comments.Comments;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-
+import java.util.ArrayList;
+import java.util.List;
 //setter x
 @Getter
 @NoArgsConstructor
@@ -15,6 +17,7 @@ public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="POST_ID")
     private Long id;
 
     @Column(length = 500, nullable = false)
@@ -26,18 +29,20 @@ public class Posts extends BaseTimeEntity {
     @Column(nullable = false)
     private String author;
 
+    @OneToMany(mappedBy = "posts")  //N:1 양방향연결 , 1:N x
+    private List<Comments> comments=new ArrayList<>();
 
 
     @Builder
-    public Posts(String title, String content, String author) {
+    public Posts(String title, String content, String author,List<Comments> comments) {
         this.title = title;
         this.content = content;
         this.author = author;
+        this.comments=comments;
     }
 
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
-
 }
